953f338268a860361ca84e9232c126f08111e2f6
amandats4i <amandatsai16@gmail.com>
2024-01-19 15:18:58 -0300

    feat: adicionar componente Navbar

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar'
+
+
+function App() {
+    return (
+        <>
+           
+            
+            <div className="min-h-[80vh]">
+            <Navbar />
+
+        
+            </div>
+            
+        </>
+    )
+}
+
+export default App
