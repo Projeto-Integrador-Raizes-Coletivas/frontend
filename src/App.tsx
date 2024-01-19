aa25c85fb5529a1351bec96c623c2c7412198d7a
Carolina <carolinasdias06@gmail.com>
2024-01-19 16:32:27 -0300

    feat: adicionar componente Footer

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Footer from "./assets/components/footer/Footer"
+
+function App() {
+    return (
+        <>
+            <div className="min-h-[80vh]">
+            <Footer/>
+            </div>
+       
+        </>
+    )
+}
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    extend: {
+      colors : {
+        "verde" : "#285d56",
+        "verde-claro" : "#c4d446",
+        "bege" : "#e3ddd8",
+        "seasalt" : "#f8f6f4",
 
+      }
+    },
+  plugins: [],
+  
+
+  }
+}
