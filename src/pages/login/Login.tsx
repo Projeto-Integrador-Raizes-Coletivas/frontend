74bd3ddac8468959f201da1d5583b50e9d6d4a1a
Gabriel Rodrigues <gabriel.rodriguesxs@gmail.com>
2024-02-05 19:27:51 -0300

    feat: implementa redesign da página de login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -6,6 +6,7 @@ import UsuarioLogin from '../../models/UsuarioLogin';
 import { RotatingLines } from 'react-loader-spinner';
 
 function Login() {
+    const logo = "/src/assets/img/raizes-logo-circle-green.png";
 
     const navigate = useNavigate();
 
@@ -34,40 +35,44 @@ function Login() {
     }
 
     return (
-        <>
-            <div className="grid grid-cols-1 lg:grid-cols-2 
-                    h-screen place-items-center font-bold ">
-                <form className="flex justify-center items-center flex-col w-1/2 gap-4"
-                    onSubmit={login}>
-                    <h2 className="text-slate-900 text-5xl ">Entrar</h2>
+        <div className='bg-bege h-screen flex items-center justify-center'>
+
+            <div className="flex items-center justify-center flex-col gap-6 bg-white w-1/3 m-auto shadow-2xl rounded-md p-8">
+
+                <Link to="/">
+                    <img src={logo} alt="Logo do Raízes Coletivas" className="max-w-36" />
+                </Link>
+
+                <form className="flex justify-center items-center flex-col w-full gap-5" onSubmit={login}>
+                    <h2 className="text-slate-900 text-3xl">Entrar na sua conta</h2>
+
                     <div className="flex flex-col w-full">
-                        <label htmlFor="usuario">Usuário</label>
                         <input
                             type="text"
                             id="usuario"
                             name="usuario"
-                            placeholder="Usuario"
-                            className="border-2 border-slate-700 rounded p-2"
+                            placeholder="Digite seu e-mail"
+                            className="border-2 border-slate-300 rounded p-2 outline-none focus:border-verde focus:border-2"
                             value={usuarioLogin.usuario}
                             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
+
                     <div className="flex flex-col w-full">
-                        <label htmlFor="senha">Senha</label>
                         <input
                             type="password"
                             id="senha"
                             name="senha"
-                            placeholder="Senha"
-                            className="border-2 border-slate-700 rounded p-2"
+                            placeholder="Digite sua senha"
+                            className="border-2 border-slate-300 rounded p-2 outline-none focus:border-verde focus:border-2"
                             value={usuarioLogin.senha}
                             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
+
                     <button
                         type='submit'
-                        className="rounded bg-verde flex justify-center
-                                   hover:bg-verde-claro text-white w-1/2 py-2">
+                        className="rounded bg-verde flex justify-center hover:bg-verde-claro text-white w-full py-2 transition delay-75 hover:text-verde">
 
                         {isLoading ? <RotatingLines
                             strokeColor="white"
@@ -76,22 +81,21 @@ function Login() {
                             width="24"
                             visible={true}
                         /> :
-                            <span>Entrar</span>
+                            <span className='font-bold'>Entrar</span>
                         }
                     </button>
 
-                    <hr className="border-slate-800 w-full" />
+                    <hr className="border-verde w-3/5" />
 
                     <p>
                         Ainda não tem uma conta?{' '}
-                        <Link to="/cadastro" className="text-verde hover:underline">
+                        <Link to="/cadastro" className="text-verde hover:underline font-bold">
                             Cadastre-se
                         </Link>
                     </p>
                 </form>
-                <div className="fundoLogin hidden lg:block"></div>
             </div>
-        </>
+        </div >
     );
 }
 
