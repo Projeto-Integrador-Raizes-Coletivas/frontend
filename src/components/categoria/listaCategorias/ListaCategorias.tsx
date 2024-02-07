1f2eb0bff7cf9e395e39b04b16432604feaebffb
Bryan S. Vieira <bryan.vieira2013@gmail.com>
2024-02-07 09:31:17 -0300

    fix card cartegoria

--- src/components/categoria/listaCategorias/ListaCategorias.tsx
+++ src/components/categoria/listaCategorias/ListaCategorias.tsx
@@ -44,7 +44,7 @@ function ListaCategorias() {
             )}
             <div className="flex justify-center w-full my-4">
                 <div className="container flex flex-col">
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8">
                         <>
                             {categorias.map((categoria) => (
                                 <>
